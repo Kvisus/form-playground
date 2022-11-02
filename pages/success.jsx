b6baf004ac13efc823f600838e938e1c17f36d17
Kvisus <eternal.wanderer.z@gmail.com>
2022-11-02 18:26:36 +0300

    final

--- pages/success.jsx
+++ pages/success.jsx
@@ -1,14 +1,39 @@
 import { useRouter } from "next/router";
+import { motion as m } from "framer-motion";
+import Confetti from "react-confetti";
+import { useEffect, useState } from "react";
 
 const Success = () => {
+  const [pieces, setPieces] = useState(200);
   const router = useRouter();
+
+const stopConfetti = () => {
+  setTimeout(()=>{
+    setPieces(0)
+  }, 3000)
+};
+
+useEffect(()=>{
+  stopConfetti();
+},[])
+
   return (
-    <main className="h-screen flex items-center justify-center">
-      <div className="bg-white rounded-lg w-1/2 font-['Lato-Regular'] text-gray-700 p-16">
-        <h1 className="text-3xl pb-4 font-['Lato-Bold']">Thanks for Email{router.query.name}</h1>
-        <p className="">49</p>
+    <m.main
+      className="h-screen flex items-center justify-center"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="text-center bg-white rounded-lg w-1/2 font-['Lato-Regular'] text-gray-700 p-16">
+        <h1 className="text-3xl pb-4 font-['Lato-Bold']">
+          Thanks for Email{router.query.name}
+        </h1>
+        <p className="text-lg text-gray-500">
+          We have sent you an email over at {router.query.email}.
+        </p>
       </div>
-    </main>
+      <Confetti gravity={0.2} numberOfPieces={pieces}/>
+    </m.main>
   );
 };
 
